6c48c70b95c42d780571de57ea49af7d816676e5
GeigersFelix <32980135+GeigersFelix@users.noreply.github.com>
2018-05-02 21:44:38 +0200

    Wifi
    Testcodes

---
+++ Wifi-test/Netzwerke_erkennen/Netzwerke_erkennen.ino
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+
+void setup() 
+{
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+
+  int N=WiFi.scanNetworks();
+  if(N>0)
+  {
+    Serial.println("");
+    Serial.print(N);
+    Serial.println(" Networks found");
+    for (int i=0; i<N; i++)
+    {
+      Serial.print(i+1);
+      Serial.print(": ");
+      Serial.print("Network Name: ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" ");
+      Serial.print("Signal Strangth: ");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(" ");
+      Serial.print("Mac Address: ");
+      Serial.println(WiFi.BSSIDstr(i));
+      delay(50);
+    }
+  }
+}
+
+void loop() 
+{
+  
+}
---
+++ Wifi-test/Verbinden/Verbinden.ino
@@ -0,0 +1,25 @@
+#include <WiFi.h>
+
+const char* ssid = "MakerArea";
+const char* password = "!MaAr_2018@";
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("");
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Attempting to connect...");
+    delay(1000);
+  }
+  Serial.println("Connected to Wi-Fi network");
+
+  Serial.print("IP-address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() 
+{
+  
+}
